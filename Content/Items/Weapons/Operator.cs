e89409585c6f6b0ecba0333123dc01cb8947e920
Henry-Saver20 <70242641+Henry-Saver20@users.noreply.github.com>
2022-12-05 11:30:37 -0700

    Default assets for weapons

---
+++ Content/Items/Weapons/Operator.cs
@@ -0,0 +1,64 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System;
+using System.Collections.Generic;
+using Terraria.GameContent.Creative;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using TerrariaExpansionOfEverything.Content.Projectiles.Weapons;
+
+namespace TerrariaExpansionOfEverything.Content.Items.Weapons
+{
+    internal class Operator : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Operator");
+            Tooltip.SetDefault("One shot, one kill.");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            // Item.staff[Item.type] = true;
+        }
+        public override void SetDefaults(){
+            Item.height = 32;
+            Item.width = 32;
+
+
+            Item.sellPrice(gold: 50);
+            Item.value = 2500;
+            Item.maxStack = 1;
+
+            
+            Item.useTime = 20;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.useAnimation = 20;
+
+            Item.noMelee = true;
+            Item.damage = 40000;
+            Item.knockBack = 50f;
+            Item.autoReuse = true;
+
+            Item.rare = ItemRarityID.Master;
+            Item.UseSound = SoundID.DD2_LightningAuraZap;
+            Item.useAmmo = AmmoID.Bullet;
+            // Item.shoot = ProjectileID.FireArrow;
+
+            // Tool Zone
+            Item.shootSpeed = 10f;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Vector2 newPos = new Vector2(velocity.X * 1.2, velocity.Y * 1.2);
+            position += newPos;
+            // Projectile.NewProjectile(source, newPos * 20f, velocity, type, damage, knockback);
+            return true;
+        }
+        public override void AddRecipes()
+        {
+            base.AddRecipes();
+            CreateRecipe()
+                .AddIngredient(ItemID.DirtBlock, 5)
+                .Register();
+        }
+    }
+}
---
+++ Content/Projectiles/Weapons/SharpshooterRounds.cs
@@ -0,0 +1,39 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+
+namespace TerrariaExpansionOfEverything.Content.Projectiles.Weapons
+ {
+    internal class SharpshooterRounds : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 4;
+            Projectile.height = 20;
+
+            Projectile.friendly = true;
+            Projectile.tileCollide = true;
+            Projectile.ignoreWater = true;
+            
+            Projectile.DamageType = DamageClass.Magic;
+
+            Projectile.aiStyle = 1;
+
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 400;
+            Projectile.scale = 0.75f;
+            Projectile.extraUpdates = 1;
+            // Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+            
+        }
+        public override void AI()
+        {
+            Projectile.aiStyle = 0;
+            Lighting.AddLight(Projectiles.position, 0.3f, 0.3f, 0.75f);
+            Lighting.Brightness(1,1);
+        }
+    }
+}

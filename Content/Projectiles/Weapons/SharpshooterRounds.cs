dbf7df3b4b8ef3891408959957f0aafad1ce8b3c
Henry-Saver20 <70242641+Henry-Saver20@users.noreply.github.com>
2022-12-06 14:48:52 -0700

    Sharpshooter Rounds working for Vandal

--- Content/Projectiles/Weapons/SharpshooterRounds.cs
+++ Content/Projectiles/Weapons/SharpshooterRounds.cs
@@ -18,7 +18,7 @@ namespace TerrariaExpansionOfEverything.Content.Projectiles.Weapons
             Projectile.tileCollide = true;
             Projectile.ignoreWater = true;
             
-            Projectile.DamageType = DamageClass.Magic;
+            Projectile.DamageType = DamageClass.Ranged;
 
             Projectile.aiStyle = 1;
 
@@ -34,6 +34,15 @@ namespace TerrariaExpansionOfEverything.Content.Projectiles.Weapons
             Projectile.aiStyle = 0;
             Lighting.AddLight(Projectile.position, 0.3f, 0.3f, 0.75f);
             Lighting.Brightness(1,1);
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            //PlaySound(0, (int)Projectile.position.X , (int)Projectile.position.Y, 21, 0.8f, 0.8f);
+            for (var i = 0; i < 4; i++)
+            {
+                Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, 7, 0f, 0,1,  Color.Blue);
+            }
         }
     }
 }

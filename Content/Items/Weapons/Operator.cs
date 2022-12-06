dd398b22fca17dab168bbfe0fcfddab90ccde589
Henry-Saver20 <70242641+Henry-Saver20@users.noreply.github.com>
2022-12-06 15:26:26 -0700

    Operator Base

--- Content/Items/Weapons/Operator.cs
+++ Content/Items/Weapons/Operator.cs
@@ -39,18 +39,25 @@ namespace TerrariaExpansionOfEverything.Content.Items.Weapons
             Item.autoReuse = true;
 
             Item.rare = ItemRarityID.Master;
-            Item.UseSound = SoundID.DD2_LightningAuraZap;
+            Item.UseSound = SoundID.Item40;
             Item.useAmmo = AmmoID.Bullet;
             // Item.shoot = ProjectileID.FireArrow;
 
             // Tool Zone
+            Item.shoot = ModContent.ProjectileType<SharpshooterRounds>();
             Item.shootSpeed = 10f;
         }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            // Vector2 newPos = new Vector2(velocity.X * 1.2, velocity.Y * 1.2);
-            // position += newPos;
-            Projectile.NewProjectile(source, position * 3f, velocity, type, damage, knockback);
+            Vector2 newPos = new Vector2(velocity.X * 1.2f, velocity.Y * 1.2f);
+            position += newPos;
+            // velocity = new Vector2(0, 100);
+            //Sound 149
+            if(type == ModContent.ProjectileType<SharpshooterRounds>())
+            {
+                velocity = velocity * 4;
+            }
+            Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<SharpshooterRounds>(), damage, knockback);
             return true;
         }
         public override void AddRecipes()

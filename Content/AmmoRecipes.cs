52f55cd3d3331e0e4f63ea1cfbff38bb858ddef6
Henry-Saver20 <70242641+Henry-Saver20@users.noreply.github.com>
2022-12-07 11:01:50 -0700

    Ammo Recipe Factory

---
+++ Content/AmmoRecipes.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System;
+using System.Collections.Generic;
+using Terraria.GameContent.Creative;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using TerrariaExpansionOfEverything.Content.Items.Weapons;
+using TerrariaExpansionOfEverything.Content.Projectiles.Weapons;
+namespace TerrariaExpansionOfEverything.Content;
+
+public class AmmoRecipes
+{
+    public class AmmoRecipe : ModSystem
+    {
+        public override void AddRecipes()
+        {
+            base.AddRecipes();
+            Recipe newRecipe = Recipe.Create(ModContent.ItemType<SharpshooterBullet>(), 200);
+            newRecipe.AddIngredient(ItemID.DirtBlock, 1);
+            newRecipe.Register();
+        }
+    }
+}
---
+++ Content/Items/Weapons/SharpshooterBulllet.cs
@@ -0,0 +1,46 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System;
+using System.Collections.Generic;
+using Terraria.GameContent.Creative;
+using Terraria.DataStructures;
+using Microsoft.Xna.Framework;
+using TerrariaExpansionOfEverything.Content.Projectiles.Weapons;
+
+namespace TerrariaExpansionOfEverything.Content.Items.Weapons
+{
+    internal class SharpshooterBullet : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Sharpshooter Round");
+            Tooltip.SetDefault("They never saw it coming.");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            // Item.staff[Item.type] = true;
+        }
+        public override void SetDefaults()
+        {
+
+            Item.width = 40;
+            Item.height = 40;
+            Item.sellPrice(gold: 1);
+            Item.value = 25;
+            Item.maxStack = 9999;
+            Item.consumable = true;
+            
+            
+            Item.damage = 400;
+            Item.knockBack = 50f;
+            Item.ammo = AmmoID.Bullet;
+            Item.rare = ItemRarityID.Master;
+
+            // Tool Zone
+            Item.shoot = ModContent.ProjectileType<SharpshooterRounds>();
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-20, 0);
+        }
+    }
+}

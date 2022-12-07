6a3757826b22aed10e7d0212625b669809d18d47
Henry-Saver20 <70242641+Henry-Saver20@users.noreply.github.com>
2022-12-06 19:52:01 -0700

    Operator Updated and bug fixed

--- Content/Items/Weapons/Operator.cs
+++ Content/Items/Weapons/Operator.cs
@@ -20,10 +20,10 @@ namespace TerrariaExpansionOfEverything.Content.Items.Weapons
             // Item.staff[Item.type] = true;
         }
         public override void SetDefaults(){
-            Item.height = 32;
-            Item.width = 32;
+            Item.height = 64;
+            Item.width = 42;
 
-
+            Item.scale = 2f;
             Item.sellPrice(gold: 50);
             Item.value = 2500;
             Item.maxStack = 1;
@@ -39,7 +39,7 @@ namespace TerrariaExpansionOfEverything.Content.Items.Weapons
             Item.autoReuse = true;
 
             Item.rare = ItemRarityID.Master;
-            Item.UseSound = SoundID.Item40;
+            Item.UseSound = SoundID.Item149;
             Item.useAmmo = AmmoID.Bullet;
             // Item.shoot = ProjectileID.FireArrow;
 
@@ -49,23 +49,36 @@ namespace TerrariaExpansionOfEverything.Content.Items.Weapons
         }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Vector2 newPos = new Vector2(velocity.X * 1.2f, velocity.Y * 1.2f);
+            Vector2 newPos = new Vector2(velocity.X * 1.6f, velocity.Y * 1.6f);
             position += newPos;
             // velocity = new Vector2(0, 100);
             //Sound 149
             if(type == ModContent.ProjectileType<SharpshooterRounds>())
             {
-                velocity = velocity * 4;
+                velocity = velocity * 15f;
+            }
+
+            float recoil = -1f; // -1f
+            player.velocity = velocity * recoil;
+            int dustSize = 200;
+            for (var i = 0; i < 120; i++)
+            {
+                Dust.NewDust(position + new Vector2(0f, -200f), dustSize, dustSize, 31, 0.25f, 0.25f ,1,  Color.Gray);
             }
             Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<SharpshooterRounds>(), damage, knockback);
             return true;
         }
+        
         public override void AddRecipes()
         {
             base.AddRecipes();
             CreateRecipe()
                 .AddIngredient(ItemID.DirtBlock, 5)
                 .Register();
+        }
+        public override Vector2? HoldoutOffset()
+        {
+            return new Vector2(-20, 0);
         }
     }
 }
